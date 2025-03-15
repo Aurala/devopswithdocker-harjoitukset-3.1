e8e504bd356489f7dac73328f6d290f5e25b232b
Markus Aurala <markus.aurala@gmail.com>
2025-03-15 18:40:46 +0200

    Exercise 3.1, moved to its own repository

---
+++ express-app/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Universe! Thanks for Docker!</h1>')
+})
+
+const PORT = 8080
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
